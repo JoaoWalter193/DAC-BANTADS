f8651e6c823e8ad65a9018597414ebc20649d982
lvtrindade <lucasventurintrindade@gmail.com>
2025-08-13 16:58:07 -0300

    feat(cadastro): Criação da tela de cadastro

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TelaAutocadastroComponent } from './pages/tela-autocadastro/tela-autocadastro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'cadastro',
+        component: TelaAutocadastroComponent
+    }
+];
