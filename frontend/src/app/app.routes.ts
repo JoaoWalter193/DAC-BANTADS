94dcdf397daf8a3951371102f5c75bb3515d34f1
Giulio369 <giuliorvs@hotmail.com>
2025-08-16 21:37:12 -0300

    Tela Adm

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TelaAdministradorComponent } from './pages/tela-administrador/tela-administrador.component';
+import { GerentesComponent } from './pages/gerentes/gerentes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'tela-administrador',
+        component: TelaAdministradorComponent
+    }, {
+        path : 'gerentes',
+        component : GerentesComponent
+    }
+];
