c36d7c9d0a971d080201c3fca0cbd1aaaa528577
Giulio369 <giuliorvs@hotmail.com>
2025-08-16 21:54:00 -0300

    Estruturando

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,16 @@
 import { Routes } from '@angular/router';
 import { TelaAdministradorComponent } from './pages/tela-administrador/tela-administrador.component';
-import { GerentesComponent } from './pages/gerentes/gerentes.component';
+import { GerentesComponent } from './pages/tela-administrador/gerentes/gerentes.component';
 
 export const routes: Routes = [
-    {
-        path : 'tela-administrador',
-        component: TelaAdministradorComponent
-    }, {
-        path : 'gerentes',
-        component : GerentesComponent
-    }
+  {
+    path: 'tela-administrador',
+    component: TelaAdministradorComponent,
+    children: [
+      {
+        path: 'gerentes',
+        component: GerentesComponent
+      }
+    ]
+  }
 ];
--- frontend/src/app/pages/tela-administrador/tela-administrador.component.ts
+++ frontend/src/app/pages/tela-administrador/tela-administrador.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GerentesComponent } from '../gerentes/gerentes.component';
+import { GerentesComponent } from './gerentes/gerentes.component';
 
 @Component({
   selector: 'app-tela-administrador',
   templateUrl: './tela-administrador.component.html',
   standalone: true,
-  imports: [CommonModule, GerentesComponent] // IMPORTAR O GERENTES AQUI
+  imports: [CommonModule, GerentesComponent] 
 })
 export class TelaAdministradorComponent {}
