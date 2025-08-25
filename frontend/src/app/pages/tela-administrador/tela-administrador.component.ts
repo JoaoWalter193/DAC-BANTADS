bff3a6a8889e6e975c1c77dfc92e5645bbe10cea
Giulio369 <giuliorvs@hotmail.com>
2025-08-24 23:06:36 -0300

    fix: merge main -> tela-administrador

--- frontend/src/app/pages/tela-administrador/tela-administrador.component.ts
+++ frontend/src/app/pages/tela-administrador/tela-administrador.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GerentesComponent } from './gerentes/gerentes.component';
+import { GerentesComponent } from './tabela-gerentes/gerentes.component';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { ClientesComponent } from './tabela-clientes/clientes.component';
 
 @Component({
   selector: 'app-tela-administrador',
   templateUrl: './tela-administrador.component.html',
+  styleUrl: './tela-administrador.component.css',
   standalone: true,
-  imports: [CommonModule, GerentesComponent] 
+  imports: [
+    CommonModule,
+    GerentesComponent,
+    NavbarComponent,
+    ClientesComponent,
+  ],
 })
 export class TelaAdministradorComponent {}
