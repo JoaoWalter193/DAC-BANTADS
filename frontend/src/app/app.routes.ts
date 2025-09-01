3f387d2da41fcb56eff79ca93c59bd8467337fda
SoaresSophia <aishspo@gmail.com>
2025-09-01 17:55:29 -0300

    style: transferir estilos para arquivo global

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { GerentesComponent } from './pages/tela-administrador/tabela-gerentes/ge
 import { HomeCliente } from './pages/home-cliente/home-cliente';
 import { TelaAutocadastroComponent } from './pages/tela-autocadastro/tela-autocadastro.component';
 import { ClientesComponent } from './pages/tela-administrador/tabela-clientes/clientes.component';
+import { PerfilClienteComponent } from './pages/perfil-cliente/perfil-cliente.component';
 
 export const routes: Routes = [
   {
@@ -35,4 +36,8 @@ export const routes: Routes = [
     path: 'home-cliente',
     component: HomeCliente,
   },
+  {
+    path: 'perfil',
+    component: PerfilClienteComponent
+  }
 ];
