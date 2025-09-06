d5dc602bbf7164b961e47a881eb5770bff72c09b
lvtrindade <lucasventurintrindade@gmail.com>
2025-09-06 00:20:03 -0300

    fix(clientes): Aprovar e rejeitar clientes arrumado e formatação dos elementos.

--- frontend/src/app/pages/tela-administrador/tabela-aprovar-cliente/aprovar-cliente.component.ts
+++ frontend/src/app/pages/tela-administrador/tabela-aprovar-cliente/aprovar-cliente.component.ts
@@ -1,12 +1,18 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
 import { Component } from '@angular/core';
 import { Cliente } from '../../../models';
+import { FormatarCpfPipe } from '../../../pipes/formatar-cpf.pipe';
+import localePt from '@angular/common/locales/pt';
+import localePtExtra from '@angular/common/locales/extra/pt';
+
+registerLocaleData(localePt, 'pt-BR', localePtExtra);
 
 @Component({
   selector: 'app-aprovar-cliente',
-  imports: [CommonModule],
+  imports: [CommonModule, FormatarCpfPipe],
   templateUrl: './aprovar-cliente.component.html',
-  styleUrl: './aprovar-cliente.component.css'
+  styleUrl: './aprovar-cliente.component.css',
+  providers: [{ provide: 'LOCALE_ID', useValue: 'pt-BR' }],
 })
 export class AprovarClienteComponent {
   clientes: Cliente[] = [];
@@ -24,34 +30,43 @@ export class AprovarClienteComponent {
     }
 
     const clientes: Cliente[] = JSON.parse(contasString);
-    return clientes.filter(cliente => cliente.status === 'pendente');
+    return clientes.filter((cliente) => cliente.status === 'pendente');
   }
 
   calcularLimite(salario: number): number {
     if (salario < 2000) {
       return 0;
     } else {
-      return salario/2;
+      return salario / 2;
     }
   }
 
   aprovar(cpf: string) {
-    let clientes: Cliente[] = JSON.parse(localStorage.getItem('clientes_bantads') || '[]');
+    let clientes: Cliente[] = JSON.parse(
+      localStorage.getItem('clientes_bantads') || '[]'
+    );
 
-    clientes = clientes.map(cliente => {
+    clientes = clientes.map((cliente) => {
       return cliente.cpf === cpf ? { ...cliente, status: 'aprovado' } : cliente;
     });
 
     localStorage.setItem('clientes_bantads', JSON.stringify(clientes));
+
+    this.clientes = this.getContasPendentes();
   }
 
   rejeitar(cpf: string) {
-    let clientes: Cliente[] = JSON.parse(localStorage.getItem('clientes_bantads') || '[]');
+    let clientes: Cliente[] = JSON.parse(
+      localStorage.getItem('clientes_bantads') || '[]'
+    );
 
-    clientes = clientes.map(cliente => {
-      return cliente.cpf === cpf ? { ...cliente, status: 'rejeitado' } : cliente;
+    clientes = clientes.map((cliente) => {
+      return cliente.cpf === cpf
+        ? { ...cliente, status: 'rejeitado' }
+        : cliente;
     });
 
     localStorage.setItem('clientes_bantads', JSON.stringify(clientes));
+    this.clientes = this.getContasPendentes();
   }
 }
