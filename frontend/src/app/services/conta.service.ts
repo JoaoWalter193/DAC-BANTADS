6f6d7509df4c783b1db56c8d9509b30b0c6cef69
Pedro Lemos <p.a.lemos1@gmail.com>
2025-09-21 23:28:16 -0300

    <feat> R8 Extrato: adicionar logica e calculos do extrato
    Mostra o mock mas ainda com erro em datas e valor total por periodo.
    Vou revisar depois

--- frontend/src/app/services/conta.service.ts
+++ frontend/src/app/services/conta.service.ts
@@ -2,64 +2,22 @@ import { Injectable } from '@angular/core';
 import { Conta } from '../models/conta.interface';
 import { AuthService } from './auth.service';
 import { MockService } from './mock.service';
-
-// const LS_CHAVE = 'contaCliente';
+import { ExtratoDia, ExtratoTotal, TipoTransacao } from '../models';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContaService {
-
-  gerarExtrato(dataInicio: any, dataFim: any) {
-    throw new Error('Method not implemented.');
-  }
   constructor(private authService: AuthService, private mockService: MockService) {}
 
   getConta(): Conta | null {
     const session = this.authService.getUserSession();
     if (session?.user.role === 'CLIENTE') {
-      // Busca a conta mais recente do "banco de dados" persistente
+      // busca a conta mais recente do "banco de dados" persistente
       return this.mockService.findContaCpf(session.user.cpf) ?? null;
     }
     return null;
   }
-
-  // constructor() {
-  //   if (!localStorage.getItem(LS_CHAVE)) {
-  //     this.inicializarConta();
-  //   }
-  // }
-
-  // private inicializarConta() {
-  //   const contaInicial = {
-  //     cliente: {
-  //       cpf: '29404904902',
-  //       nome: 'Caio Isaac Freitas',
-  //       email: 'caio.isaac.freitas@agenziamarketing.com.br',
-  //       endereco: {
-  //         TipoEndereco: 'rua',
-  //         logradouro: 'São Januário',
-  //         numero: '437',
-  //         complemento: 'Jardim Botânico',
-  //         CEP: '80210300',
-  //         cidade: 'Curitiba',
-  //         estado: 'PR',
-  //       },
-  //       telefone: '4139594934',
-  //       salario: 5000.0,
-  //     },
-  //     numeroConta: '123456',
-  //     dataCriacao: new Date().toISOString(),
-  //     saldo: 1000.0,
-  //     limite: 3000.0,
-  //   };
-  //   localStorage.setItem(LS_CHAVE, JSON.stringify(contaInicial));
-  // }
-
-  // getConta(): Conta | null {
-  //   const dadosConta = localStorage.getItem(LS_CHAVE);
-  //   return dadosConta ? (JSON.parse(dadosConta) as Conta) : null;
-  // }
 
   sacar(valor: number): Conta {
     const session = this.authService.getUserSession();
@@ -153,5 +111,57 @@ export class ContaService {
     localStorage.setItem('currentUser', JSON.stringify(session));
 
     return contaOrigem;
+  }
+
+    gerarExtrato(dataInicio: Date, dataFim: Date): ExtratoTotal {
+    const conta = this.getConta();
+    if (!conta || !conta.transacoes) {
+      throw new Error('Nenhuma operação encontrada para esta conta.');
+    }
+
+    // ultimo segundo como data final
+    dataFim.setHours(23, 59, 59, 999);
+
+    const todasTransacoes = conta.transacoes.map(op => ({ ...op, data: new Date(op.data) }));
+
+    // calcular o saldo antes da data marcada
+    const saldoInicialPeriodo = todasTransacoes
+      .filter(op => op.data < dataInicio).reduce((saldo, op) => {
+        return op.tipo === TipoTransacao.DEPOSITO || (op.tipo === TipoTransacao.TRANSFERENCIA && op.clienteDestino === conta.cliente.nome)
+          ? saldo + op.valor
+          : saldo - op.valor;
+      },
+      //para começar do saldo base
+      conta.saldo - todasTransacoes.reduce((sum, op) => op.tipo === TipoTransacao.DEPOSITO ? sum + op.valor : sum - op.valor, 0));
+
+    const extrato: ExtratoDia[] = [];
+    let saldoConsolidado = saldoInicialPeriodo;
+
+    // diaa dia, da data inicial ate a final
+    for (let dia = new Date(dataInicio); dia <= dataFim; dia.setDate(dia.getDate() + 1)) {
+      const inicioDoDia = new Date(dia.setHours(0, 0, 0, 0));
+      const fimDoDia = new Date(dia.setHours(23, 59, 59, 999));
+
+      const operacoesDoDia = todasTransacoes.filter(op => op.data >= inicioDoDia && op.data <= fimDoDia);
+
+      // calcula o saldo do dia
+      operacoesDoDia.forEach(op => {
+        saldoConsolidado += op.tipo === TipoTransacao.DEPOSITO || (op.tipo === TipoTransacao.TRANSFERENCIA && op.clienteDestino === conta.cliente.nome)
+          ? op.valor
+          : -op.valor;
+      });
+
+      extrato.push({
+        data: new Date(inicioDoDia),
+        transacoes: operacoesDoDia.sort((a, b) => a.data.getTime() - b.data.getTime()),
+        saldoConsolidado: saldoConsolidado
+      });
+    }
+
+    return {
+      periodo: { inicio: dataInicio, fim: dataFim },
+      saldoInicial: saldoInicialPeriodo,
+      dias: extrato
+    };
   }
 }
