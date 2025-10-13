82c7bc478943a92ed53a19bda41def8a7ad790b8
SoaresSophia <aishspo@gmail.com>
2025-10-13 17:06:30 -0300

    fix: utilizar SHA-256 e criar usuários no banco de dados

--- microservicos/ms-auth/src/main/java/msauth/ms_auth/config/SecurityConfig.java
+++ microservicos/ms-auth/src/main/java/msauth/ms_auth/config/SecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
@@ -66,7 +68,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
+    @SuppressWarnings("deprecation")
+    public PasswordEncoder passwordEncoder() {
+        return new StandardPasswordEncoder();
     }
 }
