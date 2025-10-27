3ff7fc00adde6e90fd7e2f9b7b3c951959ce71c7
lvtrindade <lucasventurintrindade@gmail.com>
2025-10-27 15:32:57 -0300

    fix(api-gateway): Correção de erros para build no docker.

--- microservicos/APIGateway/src/config/service.js
+++ microservicos/APIGateway/src/config/service.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
+
 module.exports = {
-    DEVICE: process.env.DEVICE_SERVICE_URL,
-    AUTH: process.env.AUTH_SERVICE_URL,
-    CLIENTE: process.env.CLIENTE_SERVICE_URL,
-    CONTA: process.env.CONTA_SERVICE_URL,
-    GERENTE: process.env.GERENTE_SERVICE_URL,
+    AUTH: process.env.AUTH_SERVICE_URL || 'http://localhost:8082',
+    CLIENTE: process.env.CLIENTE_SERVICE_URL || 'http://localhost:8083',
+    CONTA: process.env.CONTA_SERVICE_URL || 'http://localhost:8080',
+    GERENTE: process.env.GERENTE_SERVICE_URL  || 'http://localhost:8081',
     JWT_SECRET: process.env.JWT_SECRET || 'segredo',
     RATE_LIMIT_WINDOW: parseInt(process.env.RATE_LIMIT_WINDOW || 1),
     RATE_LIMIT_MAX_REQUESTS: parseInt(process.env.RATE_LIMIT_MAX_REQUESTS || 100),
