2afc9b77e286553202a906490f50d36a61087b42
SoaresSophia <aishspo@gmail.com>
2025-11-03 18:39:36 -0300

    pre-merge

--- microservicos/Mscliente/src/main/java/mscliente/producer/RabbitMQProducer.java
+++ microservicos/Mscliente/src/main/java/mscliente/producer/RabbitMQProducer.java
@@ -3,7 +3,10 @@ package mscliente.producer;
 import mscliente.config.RabbitMQConfig;
 import mscliente.domain.AlteracaoPerfilDTO;
 import mscliente.domain.ResponseDTO;
+import mscliente.domain.AuthRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,17 +18,25 @@ public class RabbitMQProducer {
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
+
     @Value("exchangePrincipal")
     private String exchange;
 
     @Value("keySaga")
     private String routingKeySaga;
 
+    @Value("${rabbitmq.key.saga-create-auth}")
+    private String authSagaRoutingKey;
+
     public RabbitMQProducer(RabbitTemplate rabbitTemplate){
         this.rabbitTemplate = rabbitTemplate;
     }
 
-
+    public void sendAuthSaga(AuthRequest data){
+        LOGGER.info(String.format("Enviando evento AUTH_CREATE para SAGA -> Email: %s", data.email()));
+        rabbitTemplate.convertAndSend(exchange, authSagaRoutingKey, data);
+    }
 
 
     public void sendClienteSaga(ResponseDTO data){
