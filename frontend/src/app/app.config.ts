5f48430d5b50fe3e48769f47971bdcf4ac0b0f85
Giulio369 <giuliorvs@hotmail.com>
2025-11-17 19:10:21 -0300

    dashboard

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,10 +4,15 @@ import { provideNgxMask } from 'ngx-mask';
 
 import { routes } from './app.routes';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideNgxMask(),
+    provideAnimations(),
+    provideHttpClient(),
   ],
 };
