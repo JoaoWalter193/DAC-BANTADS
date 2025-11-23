8bccb57f3b06373bccfa429f1355fedab072745a
lvtrindade <lucasventurintrindade@gmail.com>
2025-11-23 17:25:36 -0300

    commit

---
+++ frontend/src/app/models/cliente/dto/melhor-cliente.dto.ts
@@ -0,0 +1,10 @@
+export interface MelhorClienteDTO {
+  cpf: string;
+  nome: string;
+  email: string;
+  salario: number;
+  endereco: string;
+  cidade: string;
+  estado: string;
+  saldo: number;
+}
