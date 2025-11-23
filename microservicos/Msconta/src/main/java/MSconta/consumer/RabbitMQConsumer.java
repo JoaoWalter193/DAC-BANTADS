982c239948ce756b29c3930e004b16d9128f8e42
SoaresSophia <aishspo@gmail.com>
2025-11-23 04:24:17 -0300

    sem gerente

--- microservicos/Msconta/src/main/java/MSconta/consumer/RabbitMQConsumer.java
+++ microservicos/Msconta/src/main/java/MSconta/consumer/RabbitMQConsumer.java
@@ -1,6 +1,6 @@
 package MSconta.consumer;
 
-
+import MSconta.config.RabbitMQConfig;
 import MSconta.domain.*;
 import MSconta.domain.DTOCqrs.AtualizarDTO;
 import MSconta.domain.DTOCqrs.SaqueDepositoDTO;
@@ -25,8 +25,6 @@ import java.util.Optional;
 @Service
 public class RabbitMQConsumer {
 
-
-    // usasdos para atualizar via CQRS
     @Autowired
     ContaRRepository contaRRepository;
 
@@ -36,12 +34,10 @@ public class RabbitMQConsumer {
     @Autowired
     MovimentacaoRRepository movimentacaoRRepository;
 
-// teste pedro alteracaoperfil
     @Autowired
     RabbitMQProducer rabbitMQProducer;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumer.class);
-
     private final ObjectMapper objectMapper;
 
     public RabbitMQConsumer() {
@@ -50,52 +46,28 @@ public class RabbitMQConsumer {
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
+    @RabbitListener(queues = RabbitMQConfig.QUEUE_CONTA_CRIAR)
+    public void processarAprovacaoConta(AprovacaoDTO saga) {
+        LOGGER.info("MS-CONTA: Recebido comando de APROVAÇÃO (Criar Conta) para CPF: {}", saga.cpf());
+        try {
+            AprovacaoDTO resultado = contaCUDService.criarContaAprovada(saga);
 
-    @RabbitListener(queues = {"MsConta"})
-    public void consume(ResponseDTO message) {
-        if (message.ms().equals("Criar conta")) {
-            AdicionarContaDTO contaTemp = new AdicionarContaDTO(message.cpf(),
-                    message.nome(),
-                    message.salario());
-            contaCUDService.adicionarConta(contaTemp);
+            if ("SUCESSO".equals(resultado.status())) {
+                LOGGER.info("MS-CONTA: Conta criada com SUCESSO! Retornando para Orquestrador.");
+            } else {
+                LOGGER.error("MS-CONTA: Falha ao criar conta. Erro: {}", resultado.mensagemErro());
+            }
+            rabbitMQProducer.enviarRespostaSaga(resultado);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            rabbitMQProducer.enviarRespostaSaga(saga.comErro(e.getMessage()));
         }
-
-
-        if (message.ms().equals("msCliente-aprovado")){
-            contaCUDService.aprovarCliente(message.cpf());
-        }
-
-
-
-
-        if (message.ms().equals("msGerente-criar")) {
-            GerenteDTO gerenteTemp = new GerenteDTO(message.cpf(), message.nome());
-            contaCUDService.adicionarGerente(gerenteTemp);
-        }
-
-        if (message.ms().equals("msGerente-deletar")) {
-            GerenteDTO gerenteTemp = new GerenteDTO(message.cpf(), message.nome());
-            contaCUDService.removerGerente(gerenteTemp);
-
-        }
-
-        if (message.ms().equals("msGerente-atualizar")){
-            GerenteDTO gerenteTemp = new GerenteDTO(message.cpf(), message.nome());
-            contaCUDService.atualizarGerente(gerenteTemp);
-        }
-
-
-
     }
 
-
-    @RabbitListener(queues = {"BancoAtt"})
+    @RabbitListener(queues = { "BancoAtt" })
     public void consumerCQRS(Map<String, Object> data) {
-
-
-
         String tipo = (String) data.get("mensagemTipo");
-
         if ("atualizar".equals(tipo)) {
             AtualizarDTO dto = objectMapper.convertValue(data, AtualizarDTO.class);
             leitorCqrsTeste(dto);
@@ -103,61 +75,59 @@ public class RabbitMQConsumer {
             SaqueDepositoDTO dto = objectMapper.convertValue(data, SaqueDepositoDTO.class);
             consumerSaqueDeposito(dto);
         } else if ("transferir".equals(tipo)) {
-
             TransferenciaDTO dto = objectMapper.convertValue(data, TransferenciaDTO.class);
             transferencia(dto);
-
         } else {
             LOGGER.warn("Mensagem desconhecida: " + data);
         }
     }
 
+    @RabbitListener(queues = { "AtualizarConta" })
+    public void atualizarLimite(AlteracaoPerfilDTO dados) {
+        ClienteDTO dadosCliente = dados.dadosAtualizados();
+        try {
+            contaCUDService.alteracaoPerfilLimite(dadosCliente.cpf(), dadosCliente.salario());
+            rabbitMQProducer.publicarEventoLimiteAtualizadoSucesso(dados);
+        } catch (Exception e) {
+            rabbitMQProducer.publicarEventoLimiteAtualizadoFalha(dados);
+        }
+    }
 
     public void leitorCqrsTeste(AtualizarDTO data) {
         try {
-            if (data.mensagemTipo().equals("atualizar")) {
-                Optional<ContaR> contaTempOpt = contaRRepository.findByCpfCliente(data.cpfCliente());
-                if (contaTempOpt.isPresent()) {
-                    ContaR contaTemp = contaTempOpt.get();
-
-                    contaTemp.setNomeCliente(data.nomeCliente());
-                    contaTemp.setNomeGerente(data.nomeGerente());
-                    contaTemp.setCpfGerente(data.cpfGerente());
-                    contaTemp.setLimite(data.limite());
-                    contaTemp.setAtiva(data.ativa());
-                    contaRRepository.save(contaTemp);
-                    LOGGER.info("Conta atualizada no banco com sucesso");
-
-                } else{
-                    ContaR contaTemp = new ContaR(data.numConta(),data.cpfCliente(), data.nomeCliente(),
-                            data.saldo(), data.limite(), data.cpfGerente(),
-                            data.nomeGerente(), data.dataCriacao());
-                    contaRRepository.save(contaTemp);
-                    LOGGER.info("Conta adicionada no banco com sucesso");
-                }
+            Optional<ContaR> contaTempOpt = contaRRepository.findByCpfCliente(data.cpfCliente());
+            if (contaTempOpt.isPresent()) {
+                ContaR contaTemp = contaTempOpt.get();
+                contaTemp.setNomeCliente(data.nomeCliente());
+                contaTemp.setNomeGerente(data.nomeGerente());
+                contaTemp.setCpfGerente(data.cpfGerente());
+                contaTemp.setLimite(data.limite());
+                contaTemp.setAtiva(data.ativa());
+                contaRRepository.save(contaTemp);
+            } else {
+                ContaR contaTemp = new ContaR(data.numConta(), data.cpfCliente(), data.nomeCliente(),
+                        data.saldo(), data.limite(), data.cpfGerente(),
+                        data.nomeGerente(), data.dataCriacao());
+                contaTemp.setAtiva(true);
+                contaRRepository.save(contaTemp);
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
     }
-
 
     public void consumerSaqueDeposito(SaqueDepositoDTO data) {
         try {
-            if (data.mensagemTipo().equals("acao")) {
-                Optional<ContaR> optContaTemp = contaRRepository.findByNumConta(String.valueOf(data.numConta()));
-                if (optContaTemp.isPresent()) {
-                    ContaR contaTemp = optContaTemp.get();
-                    contaTemp.setSaldo(data.saldo());
-                    MovimentacoesR movimentacaoTemp = new MovimentacoesR(data.dataHora(), data.tipo(),
-                            data.clienteOrigemNome(), data.clienteOrigameCpf(),
-                            data.clienteDestinoNome(), data.clieneDestinoCpf(),
-                            data.valor());
-
-                    contaRRepository.save(contaTemp);
-                    movimentacaoRRepository.save(movimentacaoTemp);
-                }
+            Optional<ContaR> optContaTemp = contaRRepository.findByNumConta(data.numConta());
+            if (optContaTemp.isPresent()) {
+                ContaR contaTemp = optContaTemp.get();
+                contaTemp.setSaldo(data.saldo());
+                MovimentacoesR movimentacaoTemp = new MovimentacoesR(data.dataHora(), data.tipo(),
+                        data.clienteOrigemNome(), data.clienteOrigameCpf(),
+                        data.clienteDestinoNome(), data.clieneDestinoCpf(),
+                        data.valor());
+                contaRRepository.save(contaTemp);
+                movimentacaoRRepository.save(movimentacaoTemp);
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -166,51 +136,23 @@ public class RabbitMQConsumer {
 
     public void transferencia(TransferenciaDTO data) {
         try {
-            if (data.mensagemTipo().equals("transferir")) {
-                Optional<ContaR> optContaTemp = contaRRepository.findByNumConta(String.valueOf(data.numConta()));
-                Optional<ContaR> optContaTemp2 = contaRRepository.findByNumConta(String.valueOf(data.numConta2()));
-
-                if (optContaTemp.isPresent() && optContaTemp2.isPresent()) {
-                    ContaR contaTemp = optContaTemp.get();
-                    contaTemp.setSaldo(data.saldo());
-
-                    ContaR contaTemp2 = optContaTemp2.get();
-                    contaTemp2.setSaldo(data.saldo2());
-
-                    MovimentacoesR movimentacaoTemp = new MovimentacoesR(data.dataHora(), data.tipo(),
-                            data.clienteOrigemNome(), data.clienteOrigameCpf(),
-                            data.clienteDestinoNome(), data.clieneDestinoCpf(),
-                            data.valor());
-
-                    contaRRepository.save(contaTemp2);
-                    contaRRepository.save(contaTemp);
-                    movimentacaoRRepository.save(movimentacaoTemp);
-                }
+            Optional<ContaR> optContaTemp = contaRRepository.findByNumConta(data.numConta());
+            Optional<ContaR> optContaTemp2 = contaRRepository.findByNumConta(data.numConta2());
+            if (optContaTemp.isPresent() && optContaTemp2.isPresent()) {
+                ContaR contaTemp = optContaTemp.get();
+                contaTemp.setSaldo(data.saldo());
+                ContaR contaTemp2 = optContaTemp2.get();
+                contaTemp2.setSaldo(data.saldo2());
+                MovimentacoesR movimentacaoTemp = new MovimentacoesR(data.dataHora(), data.tipo(),
+                        data.clienteOrigemNome(), data.clienteOrigameCpf(),
+                        data.clienteDestinoNome(), data.clieneDestinoCpf(),
+                        data.valor());
+                contaRRepository.save(contaTemp2);
+                contaRRepository.save(contaTemp);
+                movimentacaoRRepository.save(movimentacaoTemp);
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
     }
-
-
-// teste pedro alteracaoperfil
-    @RabbitListener(queues = {"AtualizarConta"})
-    public void atualizarLimite(AlteracaoPerfilDTO dados) {
-        ClienteDTO dadosCliente = dados.dadosAtualizados();
-        System.out.println("saga->conta atualizar limite");
-        
-        try {
-            contaCUDService.alteracaoPerfilLimite(dadosCliente.cpf(), dadosCliente.salario());
-            
-            System.out.println("conta->saga limite alterado suceesso");
-            rabbitMQProducer.publicarEventoLimiteAtualizadoSucesso(dados);
-
-        } catch (Exception e) {
-            System.out.println("conta->saga falha limite ");
-            rabbitMQProducer.publicarEventoLimiteAtualizadoFalha(dados);
-        }
-    }
-
-
 }
--- microservicos/Msconta/src/main/java/MSconta/producer/RabbitMQProducer.java
+++ microservicos/Msconta/src/main/java/MSconta/producer/RabbitMQProducer.java
@@ -1,7 +1,9 @@
 package MSconta.producer;
 
-
-import MSconta.domain.*;
+import MSconta.domain.ContaCUD;
+import MSconta.domain.AprovacaoDTO;
+import MSconta.domain.ResponseDTO;
+import MSconta.domain.AlteracaoPerfilDTO;
 import MSconta.domain.DTOCqrs.AtualizarDTO;
 import MSconta.domain.DTOCqrs.SaqueDepositoDTO;
 import MSconta.domain.DTOCqrs.TransferenciaDTO;
@@ -14,95 +16,72 @@ import org.springframework.stereotype.Service;
 @Service
 public class RabbitMQProducer {
 
-    @Value("exchangePrincipal")
-    private String exchange;
-    @Value("keyBanco")
-    private String routingKeyCQRS;
-
-    @Value("keySaga")
-    private String routingKeySaga;
-
-    @Value("keyCliente")
-    private String routingKeyCliente;
-
-// teste pedro alteracaopedril
-    @Value("keyAtualizacaoContaSucesso")
-    private String routingKeySagaContaSucesso;
-    @Value("keyAtualizacaoContaFalha")
-    private String routingKeySagaContaFalha;
-
-
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
+    @Value("exchangePrincipal")
+    private String exchange;
 
-    public void sendMessageCQRSAddUpdateConta(ContaCUD contaCUD){
-        AtualizarDTO dtoTemp = new AtualizarDTO(contaCUD.getNumConta(),
-                contaCUD.getCpfCliente(),
-                contaCUD.getNomeCliente(),
-                contaCUD.getSaldo(),
-                contaCUD.getLimite(),
-                contaCUD.getCpfGerente(),
-                contaCUD.getNomeGerente(),
-                contaCUD.getDataCriacao(),
-                contaCUD.isAtiva(),
-                "atualizar");
+    private static final String ROUTING_KEY_SAGA_RESPOSTA = "MsSaga-response";
 
-        rabbitTemplate.convertAndSend(exchange,routingKeyCQRS,dtoTemp);
+    @Value("keySaga")
+    private String routingKeySagaErro;
+
+    public void enviarRespostaSaga(AprovacaoDTO saga) {
+        System.out.println("MS-CONTA: Enviando resposta para: " + ROUTING_KEY_SAGA_RESPOSTA);
+        rabbitTemplate.convertAndSend(exchange, ROUTING_KEY_SAGA_RESPOSTA, saga);
     }
 
+    public void enviarRespostaSagaAprovacao(AprovacaoDTO saga) {
+        enviarRespostaSaga(saga);
+    }
+
+    public void sendMessageErroCliente(String cpf) {
+        ResponseDTO dto = new ResponseDTO(500, cpf, null, null, "Erro ms-conta -- criar cliente");
+        rabbitTemplate.convertAndSend(exchange, routingKeySagaErro, dto);
+    }
+
+
+    @Value("keyBanco")
+    private String routingKeyCQRS;
+
+    public void sendMessageCQRSAddUpdateConta(ContaCUD contaCUD) {
+        AtualizarDTO dtoTemp = new AtualizarDTO(contaCUD.getNumConta(), contaCUD.getCpfCliente(),
+                contaCUD.getNomeCliente(), contaCUD.getSaldo(), contaCUD.getLimite(),
+                contaCUD.getCpfGerente(), contaCUD.getNomeGerente(), contaCUD.getDataCriacao(),
+                true, "atualizar");
+        rabbitTemplate.convertAndSend(exchange, routingKeyCQRS, dtoTemp);
+    }
 
     public void sendMessageCQRSDepositoSaque(ContaCUD contaCUDTemp, Movimentacoes movimentacoes) {
-        SaqueDepositoDTO dtoTemp = new SaqueDepositoDTO(contaCUDTemp.getNumConta(),
-                contaCUDTemp.getSaldo(),
-                movimentacoes.getId(),
-                movimentacoes.getDataHora(),
-                movimentacoes.getTipo(),
-                movimentacoes.getClienteOrigemNome(),
-                movimentacoes.getClienteOrigemCpf(),
-                movimentacoes.getClienteDestinoNome(),
-                movimentacoes.getClienteDestinoCpf(),
-                movimentacoes.getValor(),
-                "acao");
-
-        rabbitTemplate.convertAndSend(exchange,routingKeyCQRS,dtoTemp);
+        SaqueDepositoDTO dtoTemp = new SaqueDepositoDTO(contaCUDTemp.getNumConta(), contaCUDTemp.getSaldo(),
+                movimentacoes.getId(), movimentacoes.getDataHora(), movimentacoes.getTipo(),
+                movimentacoes.getClienteOrigemNome(), movimentacoes.getClienteOrigemCpf(),
+                movimentacoes.getClienteDestinoNome(), movimentacoes.getClienteDestinoCpf(),
+                movimentacoes.getValor(), "acao");
+        rabbitTemplate.convertAndSend(exchange, routingKeyCQRS, dtoTemp);
     }
 
-    public void sendMessageCQRSTransferir(ContaCUD contaCUDTemp, Movimentacoes movimentacoes, ContaCUD contaCUDTemp2){
-        TransferenciaDTO dtoTemp = new TransferenciaDTO(contaCUDTemp.getNumConta(),
-                contaCUDTemp.getSaldo(),
-                movimentacoes.getId(),
-                movimentacoes.getDataHora(),
-                movimentacoes.getTipo(),
-                movimentacoes.getClienteOrigemNome(),
-                movimentacoes.getClienteOrigemCpf(),
-                movimentacoes.getClienteDestinoNome(),
-                movimentacoes.getClienteDestinoCpf(),
-                movimentacoes.getValor(),
-                contaCUDTemp2.getNumConta(),
-                contaCUDTemp2.getSaldo(),
-                "transferir");
-        rabbitTemplate.convertAndSend(exchange,routingKeyCQRS,dtoTemp);
+    public void sendMessageCQRSTransferir(ContaCUD contaCUDTemp, Movimentacoes movimentacoes, ContaCUD contaCUDTemp2) {
+        TransferenciaDTO dtoTemp = new TransferenciaDTO(contaCUDTemp.getNumConta(), contaCUDTemp.getSaldo(),
+                movimentacoes.getId(), movimentacoes.getDataHora(), movimentacoes.getTipo(),
+                movimentacoes.getClienteOrigemNome(), movimentacoes.getClienteOrigemCpf(),
+                movimentacoes.getClienteDestinoNome(), movimentacoes.getClienteDestinoCpf(),
+                movimentacoes.getValor(), contaCUDTemp2.getNumConta(), contaCUDTemp2.getSaldo(), "transferir");
+        rabbitTemplate.convertAndSend(exchange, routingKeyCQRS, dtoTemp);
     }
 
-    public void sendMessageErroCliente(String cpf){
+    @Value("keyAtualizacaoContaSucesso")
+    private String routingKeySagaContaSucesso;
 
-        ResponseDTO dto = new ResponseDTO(500,cpf,
-                null,null,
-                "Erro ms-conta -- criar cliente");
-        rabbitTemplate.convertAndSend(exchange, routingKeySaga,dto);
+    @Value("keyAtualizacaoContaFalha")
+    private String routingKeySagaContaFalha;
 
-    }
-
-
-// teste pedro alteracaoperfil
     public void publicarEventoLimiteAtualizadoSucesso(AlteracaoPerfilDTO dados) {
-        System.out.println("conta->saga teste sucesso ");
         rabbitTemplate.convertAndSend(exchange, routingKeySagaContaSucesso, dados);
     }
 
     public void publicarEventoLimiteAtualizadoFalha(AlteracaoPerfilDTO dados) {
-        System.out.println("conta->saga teste falha");
         rabbitTemplate.convertAndSend(exchange, routingKeySagaContaFalha, dados);
     }
 }
